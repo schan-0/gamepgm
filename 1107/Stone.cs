5ca4218bba490065dd50de7e1eb18c773f2ce7f4
schan-0 <schadenf7@gmail.com>
2024-12-12 08:06:58 +0900

    Add files via upload

---
+++ 1107/Stone.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+public class Stone : MonoBehaviour
+{
+    Vector3 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Ball").transform.position; //위치 지정
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, 0.4f); //target 방향으로 움직이기
+        transform.Rotate(new Vector3(0, 0, 5)); //돌 회전하기
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log(collider.gameObject.name);
+        if (collider.gameObject.name == "Ball")
+        {
+            GameManager gmComponent = GameObject.Find("GameManager").GetComponent<GameManager>();
+            gmComponent.RestartGame();
+        }
+    }
+}
